222d171596f5a0a0553e55e764215d5c8fb81f0a
andersontecnicoprogramador <68762932+andersontecnicoprogramador@users.noreply.github.com>
2022-05-24 22:15:38 -0300

    Add files via upload

---
+++ Curso-Sintaxe-B-sica-em-JavaScript-dio-main/Curso-Sintaxe Básica em JavaScript-dio/Aula2/script2.js
@@ -0,0 +1,99 @@
+// tipos primitivos
+
+//boolean
+//console.log(VouF);
+/*var VouF = false;
+console.log(typeof(VouF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'Anderson';
+console.log(typeof(nome))*/
+
+//como declarar varialvel 
+/*var varialvel;
+console.log(varialvel)
+varialvel = 'Moreira';
+console.log(varialvel);*/
+
+/*let varialvel2 = 'Anderson';
+varialvel2 = 'Moreira';
+console.log(varialvel2);*/
+
+/*const constante ='Anderson';
+constante = 'Moreira';
+console.log(constante);*/
+
+/*var escopoGlobal ='global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalinterno = 'local';
+    console.log(escopoLocalinterno);
+}
+
+escopoLocal();*/
+
+/*var atribuicao = 'Anderson';
+
+//comparação
+var comparacao = '0'=='0';
+console.log(comparacao);
+
+//comparação identica 
+var comparacaoIdentica='0' === '0';
+console.log(comparacaoIdentica);*/
+
+/*//adicao
+var adicao = 1 + 1;
+console.log(adicao);*/
+
+/*//subtracao
+var subtracao = 2- 1;
+console.log(subtracao);*/
+
+/*// multiplicao
+var multiplicao = 2*3;
+console.log(multiplicao);*/
+
+/*//divisao real
+var divisaoReal = 6/2 ;
+console.log(divisaoReal);*/
+
+/*// divisao inteira 
+var divisaoInteira = 5 % 2 ;
+console.log(divisaoInteira);*/
+
+/*// potenciacao 
+var potenciacao = 2 ** 10;
+console.log(potenciacao)*/
+
+/*//maior que
+var maiorQue =5 > 2;
+console.log(maiorQue);*/
+
+/*//menor 
+var menorQue = 5 < 2 ;
+console.log(menorQue);*/
+
+/*// maior ou igual a
+var maiorOuIgual = 5 > 2 ;
+console.log(maiorOuIgual);*/
+
+/*// menor ou igual
+var menorOuIgual = 5 < 2 ;
+console.log(menorOuIgual);*/
+
+/*var e = true && true;
+console.log(e);*/
+
+/*var ou = false || false;
+console.log(ou);*/
+
+/*var nao = ! true;
+console.log(nao);*/
+
+
